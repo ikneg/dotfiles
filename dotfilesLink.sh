6417d9bb3b88acaddb05d7a6916228523f933e5a
Genki <0930019@gmail.com>
2018-10-17 14:53:42 +0900

    Update dotfilesLink.sh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,5 +3,4 @@ ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.vim ~/.vim
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/.tmux ~/.tmux
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
