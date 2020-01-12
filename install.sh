aa5a6e9f100df1a60817fb24da4cd5e8f6b4abf8
ikneg <0930019@gmail.com>
2020-01-12 23:08:23 +0900

    update

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #! /bin/bash -eu
 rm -rf $HOME/.zprofile
-ln -sf .dotfiles/.zprofile $HOME/.zprofile
+ln -sf $HOME/.dotfiles/.zprofile $HOME/.zprofile
 rm -rf $HOME/.zshrc
-ln -sf .dotfiles/.zshrc $HOME/.zshrc
+ln -sf $HOME/.dotfiles/.zshrc $HOME/.zshrc
 rm -rf $HOME/.zshenv
-ln -sf .dotfiles/.zshenv $HOME/.zshenv
+ln -sf $HOME/.dotfiles/.zshenv $HOME/.zshenv
 rm -rf $HOME/.tmux.conf
-ln -sf .dotfiles/.tmux.conf $HOME/.tmux.conf
+ln -sf $HOME/.dotfiles/.tmux.conf $HOME/.tmux.conf
 rm -rf $HOME/.gitignore
-ln -sf .dotfiles/.gitignore $HOME/.gitignore
+ln -sf $HOME/.dotfiles/.gitignore $HOME/.gitignore
 rm -rf $HOME/.gitconfig
-ln -sf .dotfiles/.gitconfig $HOME/.gitconfig
+ln -sf $HOME/.dotfiles/.gitconfig $HOME/.gitconfig
 rm -rf $HOME/.config/nvim
-ln -sf .dotfiles/nvim $HOME/.config/nvim
+ln -sf $HOME/.dotfiles/nvim $HOME/.config/nvim
